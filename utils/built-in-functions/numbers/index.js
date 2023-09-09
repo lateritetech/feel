f77c54999aefefd4149543f65f869e95cf448796
Yogish Shenoy <shenoy.yogish@gmail.com>
2023-09-09 06:23:20 +0530

    Added builtin function for converting from strig to number

--- utils/built-in-functions/numbers/index.js
+++ utils/built-in-functions/numbers/index.js
@@ -1,10 +1,13 @@
 const decimal = (n, scale) => n.toFixed(scale);
 
+const numeric = (n) => parseFloat(n);
+
 const floor = n => Math.floor(n);
 
 const ceiling = n => Math.ceil(n);
 
 module.exports = {
+  numeric,
   decimal,
   floor,
   ceiling,
